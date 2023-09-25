d65151c3c5b94d56780a995116ca9368c79aeed1
Yago Alexandre <yago.afonso@tcema.tc.br>
2023-09-25 09:42:58 -0300

    add: nginx-docker.yml

--- ansible/setup-vm.sh
+++ ansible/setup-vm.sh
@@ -3,9 +3,11 @@
 echo "run setup-docker.yml"
 ansible-playbook playbooks/setup-docker.yml
 
-
 echo "run setup-k3d.yml"
 ansible-playbook playbooks/setup-k3d.yml
+
+echo "run setup-nginx.yml"
+ansible-playbook playbooks/setup-nginx.yml
 
 echo "run create-cluster.yml"
 ansible-playbook playbooks/create-cluster.yml
